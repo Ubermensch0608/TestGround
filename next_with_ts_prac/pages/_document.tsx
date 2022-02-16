2d700202cffe21c8f8fec5129ba760dd1c25b961
Ubermensch0608 <melancholyee28@gmail.com>
2022-02-16 19:12:30 +0900

    Add: 프로젝트를 위한 초기 세팅 완료

---
+++ next_with_ts_prac/pages/_document.tsx
@@ -0,0 +1,36 @@
+import Document, { DocumentContext } from 'next/document';
+import { Fragment } from 'react';
+import { ServerStyleSheet } from 'styled-components';
+
+class CustomDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <Fragment>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </Fragment>
+        ),
+      };
+    } catch (error) {
+      throw error;
+    } finally {
+      sheet.seal();
+    }
+  }
+}
+
+export default CustomDocument;
