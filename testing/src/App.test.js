c58e31762609925d836ef4b0d520b205e27b4c05
Ubermensch0608 <melancholyee28@gmail.com>
2022-04-11 00:19:45 +0900

    Add: userEvent와 fireEvent의 차이 비교

--- testing/src/App.test.js
+++ testing/src/App.test.js
@@ -1,5 +1,6 @@
 import App from "./App";
 import { fireEvent, render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe("App was tested", () => {
   test("renders App component", async () => {
@@ -8,9 +9,11 @@ describe("App was tested", () => {
     await screen.findByText(/Signed in as/);
     expect(screen.queryByText(/Searched for JavaScript/)).toBeNull();
 
-    fireEvent.change(screen.getByRole("textbox"), {
-      target: { value: "JavaScript" },
-    });
+    await userEvent.type(screen.getByRole("textbox"), "JavaScript");
+
+    // fireEvent.change(screen.getByRole("textbox"), {
+    //   target: { value: "JavaScript" },
+    // });
 
     expect(screen.getByText(/Searches for JavaScript/)).toBeInTheDocument();
   });
