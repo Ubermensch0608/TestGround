340e02701a3a6caf878aa3caa6bce9d8e893362d
Ubermensch0608 <melancholyee28@gmail.com>
2022-04-20 00:44:29 +0900

    Add: hook을 이용한 test 구현

--- testing/src/App.jsx
+++ testing/src/App.jsx
@@ -1,34 +1,7 @@
-import React, { useState } from "react";
-import axios from "axios";
-import NewTodo from "components/NewTodo";
+import Counter from "components/Counter";
 
-const URL = "http://hn.algolia.com/api/v1/search";
-
-function App() {
-  const [hitsData, setHitsData] = useState([]);
-
-  const fetchDataHandler = async () => {
-    try {
-      const { data } = await axios.get(URL);
-      const fetchedHits = data.hits;
-      setHitsData(fetchedHits);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  return (
-    <div>
-      <button onClick={fetchDataHandler}>fetch</button>
-      <ul>
-        {hitsData.map((hit) => (
-          <li key={hit.objectID}>
-            <span>Title: </span>
-            <span>{hit.title ? hit.title : "----------------"}</span>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+const App = () => {
+  return <Counter />;
+};
 
 export default App;
