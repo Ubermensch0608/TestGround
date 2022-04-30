151f05e0c5bfdeab39c7c71d472cb598b6c29072
JongWon <melancholyee28@gmail.com>
2022-04-30 23:45:44 +0900

    Add: 새로운 API 구현

--- msw-next/components/AutoList.tsx
+++ msw-next/components/AutoList.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { FC, useState } from "react";
 
 const AutoList: FC<{ products: { plus_name: string }[] }> = ({ products }) => {
@@ -5,6 +6,7 @@ const AutoList: FC<{ products: { plus_name: string }[] }> = ({ products }) => {
     { plus_name: string }[]
   >([]);
   const [inputChar, setInputChar] = useState("");
+  const [data, setData] = useState<{ id: string; content: string }[]>([]);
 
   const getInputCharHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     const searchWord = event.target.value;
@@ -22,17 +24,60 @@ const AutoList: FC<{ products: { plus_name: string }[] }> = ({ products }) => {
     }
   };
 
-  return (
-    <div>
-      <input onChange={getInputCharHandler} value={inputChar} />
+  const addDataHanlder = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // const req = await axios.post("https://my.backend/post-something");
+  };
 
-      {filteredProducts.length > 0 && (
+  const updateDataHandler = async () => {
+    const { data } = await axios.get("https://my.backend/post-something");
+
+    setData(data);
+  };
+
+  console.log(data);
+  return (
+    <div style={{ display: "flex" }}>
+      <div
+        style={{
+          width: "300px",
+          minHeight: "300px",
+          border: "2px solid black",
+          padding: "10px",
+          borderRadius: "12px",
+        }}>
+        <label>검색하기</label>
+        <input onChange={getInputCharHandler} value={inputChar} />
+        {filteredProducts.length > 0 && (
+          <ul>
+            {filteredProducts.slice(0, 10).map((product, idx) => (
+              <li key={idx}>{product.plus_name}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+      <div
+        style={{
+          width: "300px",
+          minHeight: "300px",
+          border: "2px solid black",
+          padding: "10px",
+          borderRadius: "12px",
+          marginLeft: "30px",
+        }}>
+        <form onSubmit={addDataHanlder}>
+          <label>추가하기</label>
+          <input />
+          <button type="submit" onClick={updateDataHandler}>
+            업데이트
+          </button>
+        </form>
         <ul>
-          {filteredProducts.slice(0, 10).map((product, idx) => (
-            <li key={idx}>{product.plus_name}</li>
+          {data.map((value) => (
+            <li key={value.id}>{value.content}</li>
           ))}
         </ul>
-      )}
+      </div>
     </div>
   );
 };
--- msw-next/mocks/handlers.ts
+++ msw-next/mocks/handlers.ts
@@ -5,4 +5,25 @@ export const handlers = [
   rest.get("https://my.backend/product-brand", (req, res, ctx) => {
     return res(ctx.json(PRODUCT_BRAND));
   }),
+  // rest.post('https://my.backend/product-brand', (req,res,ctx)=> {
+  //   return res(ctx.json)
+  // })
+  rest.get("https://my.backend/post-something", (req, res, ctx) => {
+    return res(
+      ctx.json([
+        {
+          id: "n1",
+          content: "react",
+        },
+        {
+          id: "n2",
+          content: "next",
+        },
+        {
+          id: "n3",
+          content: "typescript",
+        },
+      ])
+    );
+  }),
 ];
