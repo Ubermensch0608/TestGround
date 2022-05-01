8f22fd3d4c8ce741a852243c47e29690f6abb9ce
JongWon <melancholyee28@gmail.com>
2022-05-01 23:18:03 +0900

    Add: msw - post 구혀

--- msw-next/components/AutoList.tsx
+++ msw-next/components/AutoList.tsx
@@ -1,12 +1,19 @@
 import axios from "axios";
-import React, { FC, useState } from "react";
+import React, { FC, useRef, useState } from "react";
+import { nanoid } from "nanoid";
+
+export interface DataTypes {
+  id: string;
+  content: string;
+}
 
 const AutoList: FC<{ products: { plus_name: string }[] }> = ({ products }) => {
   const [filteredProducts, setFilteredProducts] = useState<
     { plus_name: string }[]
   >([]);
   const [inputChar, setInputChar] = useState("");
-  const [data, setData] = useState<{ id: string; content: string }[]>([]);
+  const [data, setData] = useState<DataTypes[]>([]);
+  const dataInputRef = useRef<HTMLInputElement>(null);
 
   const getInputCharHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     const searchWord = event.target.value;
@@ -26,7 +33,25 @@ const AutoList: FC<{ products: { plus_name: string }[] }> = ({ products }) => {
 
   const addDataHanlder = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    // const req = await axios.post("https://my.backend/post-something");
+
+    const currentInputValue = dataInputRef.current!.value;
+
+    try {
+      const response = await fetch("https://my.backend/post-something", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+        body: JSON.stringify(currentInputValue),
+      });
+
+      console.log(response);
+      return response.json();
+    } catch (error) {
+      console.error(error);
+    }
+
+    dataInputRef.current!.value = "";
   };
 
   const updateDataHandler = async () => {
@@ -67,7 +92,7 @@ const AutoList: FC<{ products: { plus_name: string }[] }> = ({ products }) => {
         }}>
         <form onSubmit={addDataHanlder}>
           <label>추가하기</label>
-          <input />
+          <input ref={dataInputRef} />
           <button type="submit" onClick={updateDataHandler}>
             업데이트
           </button>
--- msw-next/mocks/handlers.ts
+++ msw-next/mocks/handlers.ts
@@ -1,13 +1,22 @@
 import { rest } from "msw";
 import PRODUCT_BRAND from "data/product-brand-info.json";
+import { nanoid } from "nanoid";
 
 export const handlers = [
   rest.get("https://my.backend/product-brand", (req, res, ctx) => {
     return res(ctx.json(PRODUCT_BRAND));
   }),
-  // rest.post('https://my.backend/product-brand', (req,res,ctx)=> {
-  //   return res(ctx.json)
-  // })
+
+  rest.post("https://my.backend/post-something", (req, res, ctx) => {
+    return res(
+      ctx.status(201),
+      ctx.json({
+        id: "e6f36a",
+        content: "test",
+      })
+    );
+  }),
+
   rest.get("https://my.backend/post-something", (req, res, ctx) => {
     return res(
       ctx.json([
