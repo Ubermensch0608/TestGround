e10bed63ca15cc3ef6876f6ecd348c3d270a320b
JongWon <melancholyee28@gmail.com>
2022-05-03 00:03:34 +0900

    Add: react-query example

--- react-query/src/App.js
+++ react-query/src/App.js
@@ -1,5 +1,35 @@
-function App() {
-  return <div></div>;
-}
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+
+const queryClient = new QueryClient();
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Example />
+    </QueryClientProvider>
+  );
+};
 
 export default App;
+
+const Example = () => {
+  const { isLoading, error, data } = useQuery("repoData", () =>
+    fetch("https://api.github.com/repos/tannerlinsley/react-query").then(
+      (res) => res.json()
+    )
+  );
+
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <p>{data.description}</p>
+      <strong>👀 {data.subscribers_count}</strong>{" "}
+      <strong>✨ {data.stargazers_count}</strong>{" "}
+      <strong>🍴 {data.forks_count}</strong>
+    </div>
+  );
+};
